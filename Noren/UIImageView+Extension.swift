3a56bd5dcc3079d15e70a4506cbb1c535641509b
ryochan1101 <ryo1101@mercari.com>
2016-06-06 19:32:14 +0900

    Image Downloader

---
+++ Noren/UIImageView+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIImage+Extension.swift
+//  Noren
+//
+//  Created by mercari on 6/6/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func downloadImageFromUrl(url: String) {
+        guard let url = NSURL(string: url) else { return }
+        
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? NSHTTPURLResponse where httpURLResponse.statusCode == 200,
+                let mimeType = response?.MIMEType where mimeType.hasPrefix("image"),
+                let data = data where error == nil,
+                let image = UIImage(data: data) else { return }
+            
+            dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                self.image = image
+            }
+        }.resume()
+    }
+}
